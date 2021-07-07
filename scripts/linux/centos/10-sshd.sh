ac16e6cfe586d5baeee2369a8fbfe52d2d7188e4
Sam Perrin <samperrin92@gmail.com>
2021-07-07 11:32:55 +0100

    changes to cloud-init script

--- scripts/linux/centos/10-sshd.sh
+++ scripts/linux/centos/10-sshd.sh
@@ -3,7 +3,8 @@
 echo '> Configuring SSH for Public Key Authentication ...'
 
 # Disable root login via SSH
-sudo sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
+#sudo sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
+# Removed 7th July 2022, contradicts what cloud-init script does by enabling root login
 
 # Enable public key authentication
 sudo sed -i "s/.*PubkeyAuthentication.*/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
--- scripts/linux/centos/20-cloudinit.sh
+++ scripts/linux/centos/20-cloudinit.sh
@@ -7,31 +7,36 @@ sudo yum install -y cloud-init
 # Disable cloud-init on boot
 sudo touch /etc/cloud/cloud-init.disabled
 
-# Enable SSH password auth
-sed -i 's/^ssh_pwauth:   0/ssh_pwauth:   1/g' /etc/cloud/cloud.cfg
+# Enable SSH password auth and root login
+sudo sed -i 's/^ssh_pwauth:   0/ssh_pwauth:   1/g' /etc/cloud/cloud.cfg
+sudo sed -i 's/^disable_root: 1/disable_root: 0/g' /etc/cloud/cloud.cfg
 
 # Remove user section
 sudo sed -i -e '/users:/,+1d' /etc/cloud/cloud.cfg
 
-# Disable EC2 callback and restrict datasource list
-sudo sed -i '1 i\disable_ec2_metadata: True' /etc/cloud/cloud.cfg
-sudo sed -i '1 i\datasource_list: ["OVF", "None"]' /etc/cloud/cloud.cfg
-
 echo '> Disabling VMware Customisation...'
 # Disable VMware customization to facilitate static IP address assignment
-sudo sed -i "s/^disable_vmware_customization: false/disable_vmware_customization: true/" /etc/cloud/cloud.cfg
+sudo sed -i "s/^disable_vmware_customization: false/disable_vmware_customization: true/g" /etc/cloud/cloud.cfg
 # Disable network configuration if VMware customization is true
-sudo sed -i "/disable_vmware_customization: true/a\\\nnetwork:\n  config: disabled" /etc/cloud/cloud.cfg
+sudo sed -i "/disable_vmware_customization: true/a\network:" /etc/cloud/cloud.cfg
+sudo sed -i '/^network:/a\  config: disabled' /etc/cloud/cloud.cfg
+
+# Disable EC2 callback and restrict datasource list
+sudo sed -i '1 i\disable_ec2_metadata: True' /etc/cloud/cloud.cfg
+# Set the datasource as OVF only 
+sudo sed -i '1 i\datasource_list: ["OVF", "None"]' /etc/cloud/cloud.cfg
+#sed -i '/^disable_vmware_customization: true/a\datasource_list: [OVF]' /etc/cloud/cloud.cfg
 
 # Disable /tmp folder clearing (using @ as separator)
+SOURCE_TEXT="v /tmp 1777 root root 10d"
+DEST_TEXT="#v /tmp 1777 root root 10d"
+sudo sed -i "s@${SOURCE_TEXT}@${DEST_TEXT}@g" /usr/lib/tmpfiles.d/tmp.conf
+sudo sed -i "s/\(^.*10d.*$\)/#\1/" /usr/lib/tmpfiles.d/tmp.conf
 sudo sed -i "s@^[a-z] /tmp @# &@" /usr/lib/tmpfiles.d/tmp.conf
 
 ### Add After=dbus.service to VMware Tools daemon. ### 
 echo '> Adding After=dbus.service to VMware Tools daemon ...'
 sudo sed -i '/^After=vgauthd.service/a\After=dbus.service' /usr/lib/systemd/system/vmtoolsd.service
-
-# Set the datasource as OVF only 
-sed -i '/^disable_vmware_customization: true/a\datasource_list: [OVF]' /etc/cloud/cloud.cfg
 
 # Create runonce.sh script
 cat << RUNONCE > /etc/cloud/runonce.sh
@@ -49,6 +54,8 @@ sudo touch /etc/cloud/cloud-init.disabled
 sudo touch /tmp/cloud-init.complete
 sudo crontab -r
 sudo eject --cdrom
+# Restart sshd
+sudo systemctl restart sshd
 RUNONCE
 
 # Set execute permissions on runonce.sh
